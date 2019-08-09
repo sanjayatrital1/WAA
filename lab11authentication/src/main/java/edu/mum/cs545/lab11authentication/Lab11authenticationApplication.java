d0fda51527f900601172d0b337d90101b496a314
sanjayatrital1 <snjtrital@gmail.com>
2019-08-09 07:41:16 -0500

    lab11 added

---
+++ lab11authentication/src/main/java/edu/mum/cs545/lab11authentication/Lab11authenticationApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs545.lab11authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab11authenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab11authenticationApplication.class, args);
+    }
+
+}
---
+++ lab11authentication/src/main/java/edu/mum/cs545/lab11authentication/ServletInitializer.java
@@ -0,0 +1,13 @@
+package edu.mum.cs545.lab11authentication;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Lab11authenticationApplication.class);
+    }
+
+}
